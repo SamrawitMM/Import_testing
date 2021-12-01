64d129d6a94c15d74897f3ded0902632f5f1f24a
SamrawitMM <samrawitmulu88@gmail.com>
2021-12-01 13:22:50 -0500

    updated import testing

--- app_test/App.js
+++ app_test/App.js
@@ -35,126 +35,126 @@ import RadioForm from 'react-native-simple-radio-button';
 
 class App extends Component {
 
-	constructor(props){  
-     	super(props);  
-     	this.state = {
-     		providers: null,
-     		
-     	}
-	} 
-	
- componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      providers: null,
+
+    }
+  }
+
+  componentDidMount() {
     const { providers } = this.state;
     let api = getListofServiceProviders();
     api
       .then(response => {
         //console.log('getting data from axios', response);
         //setTimeout(() => {
-	console.log('getting data from axios', response);
-	this.setState({
+        console.log('getting data from axios', response);
+        this.setState({
           providers: response
         })
-	//}, 90000);
-       
+        //}, 90000);
+
       })
       .catch(error => {
         console.log(error);
-         if (error.response) {
-      // Request made and server responded
-      console.log("Request made and server responded" );
-      console.log("data is " + error.response.data);
-      console.log("status code is " + error.response.status);
-      console.log("header is" + error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.log("The request was made but no response was received" );
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Something happened in setting up the request that triggered an Error" );
-      console.log('Error', error.message);
-    }
-        
-        
-        
-        
-      });
-} 
+        if (error.response) {
+          // Request made and server responded
+          console.log("Request made and server responded");
+          console.log("data is " + error.response.data);
+          console.log("status code is " + error.response.status);
+          console.log("header is" + error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log("The request was made but no response was received");
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Something happened in setting up the request that triggered an Error");
+          console.log('Error', error.message);
+        }
 
-// #################
-onProviderSelect = (value) => {
+
+
+
+      });
+  }
+
+  // #################
+  onProviderSelect = (value) => {
     this.setState({ selectedProvider: value });
     console.log(value + " you have selected the value id ")
 
   }
 
-delayedPromise(ms, value) {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(value), ms);
-  });
-}
+  delayedPromise(ms, value) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(value), ms);
+    });
+  }
 
 
 
   renderPaymentMethods = () => {
-    const { providers, fake_api } = this.state;
-   
-      const radioProps = providers ? providers.map(provider => ({
-        label: provider.task,
-        value: provider._id,
-      })) : <Text> Loading ... </Text>
+    const { providers } = this.state;
 
-      return (
-        <RadioForm
-          radio_props={radioProps}
-          initial={0}
-          animation={false}
-          onPress={value => {
-            this.onProviderSelect(value);
-          }}
-        />
-      );
-    
+    const radioProps = providers ? providers.map(provider => ({
+      label: provider.task,
+      value: provider._id,
+    })) : <Text> Loading ... </Text>
+
+    return (
+      <RadioForm
+        radio_props={radioProps}
+        initial={0}
+        animation={false}
+        onPress={value => {
+          this.onProviderSelect(value);
+        }}
+      />
+    );
+
 
 
   }
 
   // ####################
-  
+
   paymentMethod = () => {
-  	const { providers } = this.state;
-  	if(providers){
-  	 providers.map((provider, index) => {
-                 return(
-                     <Text key={provider._id}>{provider.task}</Text>
-                        
-                 )
-             })  
-             }
-             else {
-              return (<Text>Loading ...</Text>)
-             }
+    const { providers } = this.state;
+    if (providers) {
+      providers.map((provider, index) => {
+        return (
+          <Text key={provider._id}>{provider.task}</Text>
+
+        )
+      })
+    }
+    else {
+      return (<Text>Loading ...</Text>)
+    }
   }
-  
-   render() {
-       const { providers } = this.state;
-   	return(
-   	
-   		<View>
-   		{
+
+  render() {
+    const { providers } = this.state;
+    return (
+
+      <View>
+        {
 
           this.renderPaymentMethods()
-         //console.log( this.delayedPromise(5000, 'hey'))
+          //console.log( this.delayedPromise(5000, 'hey'))
         }
-        
 
-   		</View>
-   	
-   	
-   	)
-   
-   
-   }
+
+      </View>
+
+
+    )
+
+
+  }
 
 }
 
